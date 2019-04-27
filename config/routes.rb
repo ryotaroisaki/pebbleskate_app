c31aec1fc260cae30050b7f36c4dd29015116d79
ryotaro.isaki <mdask8ryotaro@gmail.com>
2019-04-27 12:25:14 +0000

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   get 'toppages/index'
 
   root to: "toppages#index"
+  
+
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :create]
 end
